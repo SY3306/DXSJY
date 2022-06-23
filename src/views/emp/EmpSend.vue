5d54ec256823046058bda5091a657c05eafa4314
sy <772490865@qq.com>
2022-06-23 12:42:00 +0800

    完善在投递中可以删除当前投递项

--- src/views/emp/EmpSend.vue
+++ src/views/emp/EmpSend.vue
@@ -1,10 +1,10 @@
-<template>
++<template>
   <div>
     <van-nav-bar>
       <template #left>
         <van-icon name="arrow-left" size="20" @click="onClickLeft" />
       </template>
-      <template #title >我的投递</template>
+      <template #title>我的投递</template>
       <!-- <template #right>
     <van-icon name="search" size="18" />
   </template> -->
@@ -12,15 +12,16 @@
 
     <div v-if="!data"><span>还没有投递简历哦！快去寻找心仪的工作吧</span></div>
     <div v-else>
-      <lqfindex v-for="jl in data" :item="jl" :key="jl.id" />
+      <sendindex v-for="jl in data" :item="jl" :key="jl.id" />
     </div>
   </div>
 </template>
 
 <script>
-import Lqfindex from "@/components/Lqfindex.vue";
+import sendindex from "@/components/sendindex.vue";
+
 export default {
-  components: { Lqfindex },
+  components: { sendindex },
   data() {
     return {
       data: "",
@@ -38,11 +39,20 @@ export default {
       this.data = res.data.result;
     });
   },
+  watch:{
+    data(){
+       let url = "/sendlist?uid=" + this.$store.state.id;
+    this.axios.get(url).then((res) => {
+      console.log(res);
+      this.data = res.data.result;
+    });
+    }
+  }
 };
 </script>
 
 <style scoped>
-.title{
+.title {
   font-size: 20px;
 }
 </style>
